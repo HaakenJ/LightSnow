3eb76d8ccc4b8dcf8c38809318f4f7365df932ff
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-25 09:09:59 -0800

    Changed background image, added logo icon

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,6 +5,9 @@ function Nav() {
     return (
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">
+                <span className="logo">
+                    <i class="far fa-snowflake"></i>
+                </span>
                 {' '}
                 LightSnow
             </Navbar.Brand>
