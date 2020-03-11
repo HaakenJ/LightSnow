a6f0ce0a5d2052fc873369f6641fe8375e82040a
Haakasaurus <kramerhjohnson@gmail.com>
2020-03-10 21:05:56 -0700

    Made dropown containers flex, added margin

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
+import "./style.css";
 
 function Menu({
     title,
@@ -9,34 +10,36 @@ function Menu({
     subtitle2
 }) {
     return (
-        <div className="dropdown-container">
+        <div>
             <h3>{title}</h3>
-            <Dropdown>
-                <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                    {subtitle}
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu style={{ maxHeight: "200px", overflowY: "scroll", float: "left" }}>
-                    {items.map(item => 
-                        <Dropdown.Item href="#">{item}</Dropdown.Item>
-                    )}
-                </Dropdown.Menu>
-            </Dropdown>
-
-            { items2 ? 
+            <div className="drop-container">
                 <Dropdown>
                     <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                        {subtitle2}
+                        {subtitle}
                     </Dropdown.Toggle>
-    
-                    <Dropdown.Menu style={{ maxHeight: "200px", overflowY: "scroll" }}>
-                        {items2.map(item => 
+
+                    <Dropdown.Menu style={{ maxHeight: "200px", overflowY: "scroll", float: "left" }}>
+                        {items.map(item => 
                             <Dropdown.Item href="#">{item}</Dropdown.Item>
                         )}
                     </Dropdown.Menu>
                 </Dropdown>
+
+                { items2 ? 
+                    <Dropdown className="ml-2">
+                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                            {subtitle2}
+                        </Dropdown.Toggle>
+        
+                        <Dropdown.Menu style={{ maxHeight: "200px", overflowY: "scroll" }}>
+                            {items2.map(item => 
+                                <Dropdown.Item href="#">{item}</Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
                     : ""
                 }
+            </div>
         </div>
     )
 }
