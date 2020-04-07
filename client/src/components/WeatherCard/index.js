140482c4aabecbbbc12473bff51366cccf1ca6db
Haakasaurus <kramerhjohnson@gmail.com>
2020-04-06 18:54:56 -0700

    added body and title props to weather card

--- client/src/components/WeatherCard/index.js
+++ client/src/components/WeatherCard/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Card from "react-bootstrap";
 
-function WeatherCard() {
+function WeatherCard(
+    title,
+    body
+) {
     return (
         <Card>
             <Card.body>
-                
+                <Card.Title>
+                    {title}
+                </Card.Title>
+                <Card.Text>
+                    {body}
+                </Card.Text>
             </Card.body>
         </Card>
     )
